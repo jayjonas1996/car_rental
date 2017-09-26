37d419b417b32cd788690e8e7f3a8d78f467260a
Jay Naik <jayjonas1996@outlook.com>
2017-09-27 03:22:58 +0530

    Add project files.

---
+++ car_rental/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace car_rental
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            string a = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename='";
+            string b = Server.MapPath("\\App_Data\\Database1.mdf';Integrated Security=True");
+            Application["constr_microsoft"] = a + b;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
